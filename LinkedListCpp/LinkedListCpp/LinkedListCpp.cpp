b8a23ed412a3991bc5ed1a1f59f537b806faf6da
RickSanchezzz666 <lapiyktube@gmail.com>
2023-11-17 12:41:17 +0200

    LinkedList Completed!

--- LinkedListCpp/LinkedListCpp/LinkedListCpp.cpp
+++ LinkedListCpp/LinkedListCpp/LinkedListCpp.cpp
@@ -15,7 +15,7 @@ private:
 	Node* __createHead() {
 		if (__count <= -1) {
 			Node* head = new Node;
-			++__count;
+			__count = 0;
 			return head;
 		}
 	}
@@ -40,23 +40,51 @@ public:
 		}
 	}
 
-	void addNode(int newData) {
+	void addNode(int newData, int nodePos = -1) {
 		if (__isListFull() == true) { 
 			std::cout << "The List is Full!\n";
 			return; 
 		}
-		if (__count <= -1) {
-			__createHead();
+		if (nodePos == -1) {
+			if (__count <= -1) {
+				__createHead();
+			}
+			Node* newNode = new Node;
+			Node* node = __head;
+			while (node->next != nullptr) {
+				node = node->next;
+			}
+			node->data = newData;
+			node->next = newNode;
+			++__count;
 		}
-		Node* newNode = new Node;
-		Node* node = __head;
-		while (node->next != nullptr) {
-			node = node->next;
-		}
+		else {
+			if (nodePos > __count || nodePos < 1) {
+				std::cout << "The Node was not found!\n";
+				return; 
+			} else {
+				Node* newNode = new Node;
+				newNode->data = newData;
 
-		node->data = newData;
-		node->next = newNode;
-		++__count;
+				if (__count <= -1) {
+					__createHead();
+				}
+				Node* node = __head;
+				for (int i = 1; i < nodePos; i++) {
+					if (node->next != nullptr) {
+						node = node->next;
+					}
+					else {
+						std::cout << "The Node was not found!\n";
+						delete newNode;
+						return;
+					}
+				}
+				newNode->next = node->next;
+				node->next = newNode;
+				++__count;
+			};
+		}
 	}
 
 	void deleteNode() {
@@ -84,12 +112,29 @@ public:
 
 	}
 
+	Node* searchNode(int nodePos) {
+		if (nodePos > __count || nodePos < 1) {
+			std::cout << "The Node was not found!\n";
+			return nullptr;
+		} else {
+			Node* node = __head;
+			for (int i = 1; i < nodePos; i++) {
+				if (node->next != nullptr) {
+					node = node->next;
+				} else {
+					std::cout << "The Node was not found!\n";
+					return nullptr;
+				}
+			}
+			return node;
+		}
+	}
+
 	void displayList() {
 		if (__isListEmpty() == true) {
 			std::cout << "The List is Empty!\n";
 			return; 
-		}
-		else {
+		} else {
 			std::cout << "Your List is:\nHEAD";
 			Node* node = __head;
 			for (int i = 0; i < __count; i++) {
@@ -99,20 +144,68 @@ public:
 			std::cout << std::endl;
 		}
 	}
+
+	void sortList() {
+		if (__isListEmpty()) {
+			std::cout << "The List is Empty!\n";
+			return;
+		}
+
+		bool swapped;
+		do {
+			swapped = false;
+			Node* previous = nullptr;
+			Node* current = __head;
+			Node* next = current->next;
+
+			while (next != nullptr) {
+				if (current->data > next->data) {
+					if (previous != nullptr) {
+						previous->next = next;
+					}
+					else {
+						__head = next;
+					}
+
+					current->next = next->next;
+					next->next = current;
+
+					previous = next;
+					next = current->next;
+
+					swapped = true;
+				}
+				else {
+					previous = current;
+					current = next;
+					next = next->next;
+				}
+			}
+		} while (swapped);
+
+		displayList();
+	}
+
 };
 
 int main()
 {
 	LinkedList list;
 	
-	list.addNode(5);
+	list.addNode(31);
 	list.addNode(11);
 	list.addNode(15);
 	list.addNode(105);
-	list.addNode(26);
+	list.addNode(26, 2);
 
 	list.deleteNode();
 
+	Node* node = list.searchNode(3);
+
 	list.displayList();
+
+	std::cout << "My value: " << node->data << std::endl;
+
+	list.sortList();
 	return 0;
 }
